4fc543cb85e090b10752c00f23df6b4b39b5bfa9
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2023-07-29 18:58:14 +0530

    Add files via upload

---
+++ 43-dynamicProgramming/aFibonacchiRec.java
@@ -0,0 +1,19 @@
+public class aFibonacchiRec {
+    public static int fib(int n){
+        if(n==0 || n==1) {
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static void printFibSeries(int n){
+        System.out.print("Fibonacchi Series for n[" + n + "] ==> ");
+        for(int i=0; i<n; i++){
+            System.out.print(fib(i) + " ");
+        }
+    }
+    public static void main(String args[]){
+        int n=5;
+        System.out.println("fib at idx[" + n + "] ==> " + fib(n));
+        printFibSeries(n);
+    }
+}

226ab2118951126f68948109abb33689f4e7ce72
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2023-08-08 02:34:05 +0530

    Add files via upload

--- 29_binaryTree/b6_diameter.java
+++ 29_binaryTree/b6_diameter.java
@@ -14,11 +14,10 @@ public class b6_diameter {
     }
     
     static class binaryTree{
-
         static int idx=-1;
-
         public static node build(int nodes[]){
             idx++;
+            
             if(nodes[idx]==-1){
                 return null;
             }
@@ -31,6 +30,7 @@ public class b6_diameter {
     }
    
     static void preOrder(node root){
+        
         if(root == null){
             System.out.print("-1 ");
             return;
@@ -42,6 +42,7 @@ public class b6_diameter {
     }
     
     static int ht(node root){
+        
         if(root == null){
             return 0;
         }
@@ -51,16 +52,41 @@ public class b6_diameter {
         return Math.max(lht , rht)  + 1;
     }
     
-    static int diam(node root){
+    static int diam2(node root){
         if(root == null){
             return 0;
         }
-        int ld = diam(root.left);
+        int ld = diam2(root.left);
         int lht = ht(root.left);
-        int rd = diam(root.right);
+        int rd = diam2(root.right);
         int rht = ht(root.right);
 
         return Math.max(Math.max(ld,rd) , lht+rht+1);
+    }
+    
+    static class info{
+        int di;
+        int ht;
+
+        public info(int d, int h){
+            this.di = d;
+            this.ht = h;
+        }
+    }
+    static info diam(node root){
+
+        if(root == null){
+            return new info(0,0);
+        }
+        
+        info linfo = diam(root.left);
+        info rinfo = diam(root.right);
+
+
+        int diam = Math.max(Math.max(linfo.di,rinfo.di), linfo.ht + rinfo.ht + 1);
+        int ht = Math.max(linfo.ht, rinfo.ht) + 1;
+
+        return new info(diam,ht);
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,8,9,-1,-1,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1}; //7
@@ -79,11 +105,11 @@ public class b6_diameter {
 
         binaryTree bt = new binaryTree();
         node root = bt.build(nodes);
-        preOrder(root);
+        // preOrder(root);
         System.out.println();
 
         // System.out.println(root.data);
-        System.out.println(diam(root));
+        System.out.println(diam(root).di);
 
     }
 }
